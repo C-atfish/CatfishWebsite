8753b2df518253e1b3bb12e59bea2cb81aa617f5
c-atfish <jonlar@student.kristiania.no>
2021-12-18 18:31:45 +0100

    added track info link

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var imgSpot = document.getElementById("imgPlace");
 var images = ["HeavyStepsTrackart.jpg", "FallingInReverseTrackart.jpg"];
+var trackNames = ["Heavy Steps", "Falling In Reverse"];
+var ytLinks = ["https://www.youtube.com/watch?v=s7wJv-Vof50", "https://www.youtube.com/watch?v=C8qhvFMsFOM"];
+var infoSpot = document.getElementById("trackInfo");
+
 var imgCounter = 0;
-window.onload = imgSpot.innerHTML = `<img src="img/${images[imgCounter]}" class="trackArt">`;
+window.onload = loadIndex();
 
 function nextPhoto(){
     imgCounter++;
     imgSpot.innerHTML = `<img src="img/${images[imgCounter]}" class="trackArt">`;
+    infoSpot.innerHTML =`<h3><a href="${ytLinks[imgCounter]}">${trackNames[imgCounter]}</a></h3>`;
     
     
 }
@@ -14,5 +19,11 @@ function lastPhoto(){
 
     imgCounter--;
     imgSpot.innerHTML = `<img src="img/${images[imgCounter]}" class="trackArt">`;
+    infoSpot.innerHTML =`<h3><a href="${ytLinks[imgCounter]}">${trackNames[imgCounter]}</a></h3>`;
     
+}
+
+function loadIndex(){
+    imgSpot.innerHTML = `<img src="img/${images[imgCounter]}" class="trackArt">`;
+    infoSpot.innerHTML =`<h3><a href="${ytLinks[imgCounter]}">${trackNames[imgCounter]}</a></h3>`;
 }
